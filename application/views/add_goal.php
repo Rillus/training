a7b3bc0a6d54102cb9f5139846e2fabcf63e7fec
Rillus <riley@dingdigital.co.uk>
2014-02-07 08:02:19 +0000

    Update goal set/update html

--- application/views/add_goal.php
+++ application/views/add_goal.php
@@ -6,16 +6,39 @@
 </head>
 <body>
 
-<form>
-	<select name="type">
-		<option>run</option>
-		<option>lose</option>
-		<option>gain</option>
+<form id="goal_set">
+	I want to
+	<select name="action">
+		<option value="run">run</option>
+		<option value="lose">lose</option>
+		<option value="gain">gain</option>
 	</select>
 	<input type="text" name="amount" placeholder="0">
 	<select>
-		<option>km</option>
-		<option>kg</option>
+		<option value="run_unit">km</option>
+		<option value="gain_unit">kg</option>
+		<option value="lose_unit">kg</option>
+	</select>
+
+	<?php if (type == "run"){ ?>
+		in <input type="text" name="run_time" placeholder="0"> minutes
+	<?php } else { ?>
+		my current weight is <input type="text" name="gain_lose_weight" placeholder="0"> kg
+	<?php } ?>
+</form>
+
+<form id="goal_update">
+	I've
+	<select name="action">
+		<option value="run">run</option>
+		<option value="lose">lost</option>
+		<option value="gain">gained</option>
+	</select>
+	<input type="text" name="amount" placeholder="0">
+	<select>
+		<option value="run_unit">km</option>
+		<option value="gain_unit">kg</option>
+		<option value="lose_unit">kg</option>
 	</select>
 
 	<?php if (type == "run"){ ?>
